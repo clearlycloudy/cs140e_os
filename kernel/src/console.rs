e55203e8994518634bdbdf34d54712ff8a7dc0af
Bill Liu <yuan6liu@gmail.com>
2018-04-14 22:16:55 -0700

    added implementations for assignment2.

--- kernel/src/console.rs
+++ kernel/src/console.rs
@@ -19,35 +19,42 @@ impl Console {
     /// Initializes the console if it's not already initialized.
     #[inline]
     fn initialize(&mut self) {
-        unimplemented!()
+        self.inner = Some( MiniUart::new() );
     }
 
     /// Returns a mutable borrow to the inner `MiniUart`, initializing it as
     /// needed.
     fn inner(&mut self) -> &mut MiniUart {
-        unimplemented!()
+        match self.inner {
+            None => {
+                self.initialize();
+            },
+            _ => {},
+        }
+        self.inner.as_mut().unwrap()
     }
 
     /// Reads a byte from the UART device, blocking until a byte is available.
     pub fn read_byte(&mut self) -> u8 {
-        unimplemented!()
+        let mut buf = [0];
+        self.inner().read_byte()
     }
 
     /// Writes the byte `byte` to the UART device.
     pub fn write_byte(&mut self, byte: u8) {
-        unimplemented!()
+        self.inner().write_byte( byte );
     }
 }
 
 impl io::Read for Console {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        unimplemented!()
+        <MiniUart as io::Read >::read( self.inner(), buf )
     }
 }
 
 impl io::Write for Console {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        unimplemented!()
+        <MiniUart as io::Write >::write( self.inner(), buf )
     }
 
     fn flush(&mut self) -> io::Result<()> {
@@ -57,7 +64,7 @@ impl io::Write for Console {
 
 impl fmt::Write for Console {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        unimplemented!()
+        self.inner().write_str( s )
     }
 }
 
@@ -67,9 +74,15 @@ pub static CONSOLE: Mutex<Console> = Mutex::new(Console::new());
 /// Internal function called by the `kprint[ln]!` macros.
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
-    use std::fmt::Write;
-    let mut console = CONSOLE.lock();
-    console.write_fmt(args).unwrap();
+    #[cfg(not(test))]
+    {
+        use std::fmt::Write;
+        let mut console = CONSOLE.lock();
+        console.write_fmt(args).unwrap();
+    }
+
+    #[cfg(test)]
+    { print!("{}", args); }
 }
 
 /// Like `println!`, but for kernel-space.
--- kernel/src/kmain.rs
+++ kernel/src/kmain.rs
@@ -6,18 +6,111 @@
 #![feature(decl_macro)]
 #![feature(repr_align)]
 #![feature(attr_literals)]
+
+#![feature(exclusive_range_pattern)]
+#![feature(alloc, allocator_api, global_allocator)]
+
 #![feature(never_type)]
 #![feature(ptr_internals)]
+#![feature(pointer_methods)]
 
+#[macro_use]
+#[allow(unused_imports)]
+extern crate alloc;
 extern crate pi;
 extern crate stack_vec;
+extern crate fat32;
 
+pub mod allocator;
 pub mod lang_items;
 pub mod mutex;
 pub mod console;
+pub mod cmds;
 pub mod shell;
+pub mod fs;
+
+#[cfg(not(test))]
+use allocator::Allocator;
+use fs::FileSystem;
+
+#[cfg(not(test))]
+#[global_allocator]
+pub static ALLOCATOR: Allocator = Allocator::uninitialized();
+
+pub static FILE_SYSTEM: FileSystem = FileSystem::uninitialized();
 
 #[no_mangle]
+#[cfg(not(test))]
 pub extern "C" fn kmain() {
-    // FIXME: Start the shell.
+
+    use console::{ kprintln };
+    use pi::atags::*;
+    use std::iter;
+    use fat32::traits::{ FileSystem, Entry, Dir, File };
+    
+    pi::timer::spin_sleep_ms(3000);
+
+    kprintln!( "iterating through ATAGS.." );
+
+    let mut atags : pi::atags::Atags = pi::atags::Atags::get();
+
+    let mut i = atags.current().unwrap();
+    loop {
+        match i {
+            pi::atags::Atag::Core(x) => {
+                kprintln!( "atag core: {:#?}", x );
+            },
+            pi::atags::Atag::Mem(x) => {
+                kprintln!( "atag mem: {:#?}", x );
+            },
+            pi::atags::Atag::Cmd(x) => {
+                kprintln!( "atag cmd: {:#?}", x );
+            },
+            pi::atags::Atag::None => {
+                kprintln!( "atag none " );
+            },
+            pi::atags::Atag::Unknown( x ) => {
+                kprintln!( "unknown atag: {:#?}", x );
+            },
+        }
+        match atags.next() {
+            Some( x ) => { i = x; },
+            None => { break; },
+        }
+    }
+
+    kprintln!( "initializing allocators.." );
+    
+    ALLOCATOR.initialize();
+
+    kprintln!( "initializing fs.." );
+    
+    FILE_SYSTEM.initialize();
+
+    kprintln!( "files in root: " );
+
+    let d = match FILE_SYSTEM.open( "/" ) {
+        Ok(x) => x,
+        Err(e) => {
+            kprintln!( "error opening at /: {}", e );
+            panic!();
+        }
+    };
+
+    if let Some(x) = d.as_dir() {
+        let mut entries: Vec<_> = x.entries()
+            .expect("entries interator")
+            .collect();
+        for e in entries.iter() {
+            kprintln!( "   {}", e.name() );
+        }            
+    }
+
+    let mut gpio_16_out = pi::gpio::Gpio::new(16).into_output();
+    gpio_16_out.set();
+
+    kprintln!( "starting shell.." );
+    
+    shell::shell( "~>", & FILE_SYSTEM );
+
 }
